a7a45e28c5c3ee2edb010d930e97262ed9fdb979
ssyup4259 <ssyup8717@gmail.com>
2020-04-20 22:40:52 +0900

    0420 | add model comments | SY
    Former-commit-id: 50fa1c7d52c60dacbe686f7e6f6430a5fdc5a2ec

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,7 +7,7 @@
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-
+# 영업시간
 class Bhour(models.Model):
     store = models.ForeignKey('Store', models.DO_NOTHING)
     type = models.IntegerField(blank=True, null=True)
@@ -23,30 +23,23 @@ class Bhour(models.Model):
     end_time = models.TimeField(blank=True, null=True)
     etc = models.CharField(max_length=100, blank=True, null=True)
 
-
-
-
+# 최애 음식점
 class Favorite(models.Model):
     store = models.ForeignKey('Store', models.DO_NOTHING)
     user = models.ForeignKey('User', models.DO_NOTHING)
 
-
-
-
+# 좋아요 음식점
 class Like(models.Model):
     store = models.ForeignKey('Store', models.DO_NOTHING)
     user = models.ForeignKey('User', models.DO_NOTHING)
 
-
-
+# 메뉴
 class Menu(models.Model):
     store = models.ForeignKey('Store', models.DO_NOTHING)
     menu = models.CharField(max_length=45, blank=True, null=True)
     price = models.IntegerField(blank=True, null=True)
 
-
-
-
+# 리뷰
 class Review(models.Model):
     user = models.ForeignKey('User', models.DO_NOTHING)
     store = models.ForeignKey('Store', models.DO_NOTHING)
@@ -59,9 +52,7 @@ class Review(models.Model):
     tag = models.CharField(max_length=45, blank=True, null=True)
     reg_time = models.CharField(max_length=50, blank=True, null=True)
 
-
-
-
+# 음식점
 class Store(models.Model):
     id = models.IntegerField(primary_key=True)
     store_name = models.CharField(max_length=50)
@@ -80,19 +71,15 @@ class Store(models.Model):
     purpose = models.CharField(max_length=45, blank=True, null=True)
     mood = models.CharField(max_length=45, blank=True, null=True)
 
-
+# 테그
 class Tag(models.Model):
     store = models.ForeignKey('Store', models.DO_NOTHING)
     tag = models.CharField(max_length=45, blank=True, null=True)
 
-
-
+# 사용자
 class User(models.Model):
     provider = models.CharField(max_length=20, blank=True, null=True)
     _id = models.CharField(unique=True, max_length=30, blank=True, null=True)
     nickname = models.CharField(unique=True, max_length=20)
     age = models.IntegerField(blank=True, null=True)
     gender = models.CharField(max_length=10, blank=True, null=True)
-
-
-
