b0df5d75fda95ba875732fa884ffd450b55b1ec3
ssongda9412 <ssongda9412@naver.com>
2020-04-21 19:20:21 +0900

    0421 | image_list_make | DE
    Former-commit-id: 80b899cef66bd356ff170191de2a99fa1ac55169

---
+++ frontend/src/components/detail/ImageList.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import "./ImageList.scss";
+
+import prevButton from "../../img/prevButton.png"
+import nextButton from "../../img/nextButton.png"
+
+class ImageList extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props)
+    this.state = {
+      img_list: this.props.img_list,
+      index: 0,
+    };
+  }
+
+  // 클릭으로 확인
+  changeIndex = (res) => {
+    this.setState({
+      index: parseInt(res.target.id),
+    });
+  };
+
+  // 이전 사진
+  prevIndex = () => {
+    if (this.state.index === 0) {
+      this.setState({
+        index: this.state.img_list.length - 1,
+      });
+    } else {
+      this.setState({
+        index: this.state.index - 1,
+      });
+    }
+  };
+
+  // 다음 사진
+  nextIndex = () => {
+    if (this.state.index === this.state.img_list.length - 1) {
+      this.setState({
+        index: 0,
+      });
+    } else {
+      this.setState({
+        index: this.state.index + 1,
+      });
+    }
+  };
+
+  // 최대 10개까지만 이미지 보이게 하기
+  render() {
+    return (
+      <div className="ImageList">
+        <div className="thumbnail">
+          <div id="square" className="centered">
+            <img
+              alt="food"
+              className="img"
+              src={this.state.img_list[this.state.index]}
+              onClick={() => {
+                window.open(this.state.img_list[this.state.index]);
+              }}
+            />
+          </div>
+        </div>
+        <div className="index prev_index" onClick={this.prevIndex}>
+          <img alt="prevButton" src={prevButton}></img>
+        </div>
+        <div className="index next_index" onClick={this.nextIndex}>
+          <img alt="nextButton" src={nextButton}></img>
+        </div>
+        <div className="preview">
+          {this.state.img_list.map((img, index) => (
+            <img
+              key={index}
+              id={index}
+              className="preview_img"
+              alt="food"
+              src={img}
+              onClick={this.changeIndex}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+// {
+//   this.state.stores.map((store) => (
+//     <div className="card_item" key={store.id}>
+//       <Card store={store}></Card>
+//     </div>
+//   ));
+// }
+export default ImageList;
--- frontend/src/components/routers/Detail.jsx
+++ frontend/src/components/routers/Detail.jsx
@@ -5,7 +5,9 @@ import axios from "axios";
 import AppBar from "../common/AppBar";
 import Header from "../common/Header";
 import HeaderSearch from "../common/HeaderSearch";
-import ImageList from "../common/ImageList";
+import ImageList from "../detail/ImageList";
+import ReadScore from "../detail/ReadScore";
+import Liked from "../common/Liked";
 
 class Detail extends React.Component {
   constructor(props) {
@@ -13,6 +15,18 @@ class Detail extends React.Component {
     this.state = {
       login: false,
       store: [],
+      category: [],
+      img_list: [
+        "https://s3-ap-northeast-1.amazonaws.com/dcreviewsresized/20191214013944_photo1_3b4aadf8a61f.jpg",
+        "https://s3-ap-northeast-1.amazonaws.com/dcreviewsresized/300_300_20191214013944_photo2_3b4aadf8a61f.jpg",
+        "https://s3-ap-northeast-1.amazonaws.com/dcreviewsresized/300_300_20190720012714_photo1_4190537b5b1e.jpg",
+        "https://s3-ap-northeast-1.amazonaws.com/dcreviewsresized/300_300_20190720012714_photo2_4190537b5b1e.jpg",
+        "https://s3-ap-northeast-1.amazonaws.com/dcreviewsresized/300_300_20190720012714_photo3_4190537b5b1e.jpg",
+        "https://s3-ap-northeast-1.amazonaws.com/dcreviewsresized/300_300_20190519140914_photo0_wMWmpwgnAl79.jpg",
+        "https://s3-ap-northeast-1.amazonaws.com/dcreviewsresized/300_300_20190519140914_photo1_wMWmpwgnAl79.jpg",
+        "https://s3-ap-northeast-1.amazonaws.com/dcreviewsresized/300_300_20190519020742_photo3_f894408f4c7d.jpg",
+        "https://s3-ap-northeast-1.amazonaws.com/dcreviewsresized/20170718045937_photo2_43ee64e3a8a1.jpg",
+      ],
     };
   }
 
@@ -34,15 +48,16 @@ class Detail extends React.Component {
         store_id: url[url.length - 1],
       },
     })
-      // 회원 가입 안되있는 거면
       .then((res) => {
         console.log(res.data);
         this.setState({
           store: res.data,
+          category: res.data.category.split("|"),
         });
       })
       .catch((error) => {
         console.log(error);
+        // alert("현재 정보를 받아오지 못하고 있습니다")
       });
   }
 
@@ -54,23 +69,59 @@ class Detail extends React.Component {
         <div className="Detail">
           <div className="container-fluid">
             <div className="row">
-              <div className="store_image col-12 col-md-7">
-                <ImageList></ImageList>
+              <div className="store_image col-12 col-md-8">
+                {this.state.store.img !== null &&
+                this.state.store.img !== undefined ? (
+                  <ImageList img_list={this.state.store.img}></ImageList>
+                ) : (
+                  <ImageList img_list={this.state.img_list}></ImageList>
+                )}
               </div>
-              <div className="col-12 col-md-5">
+              <div className="col-12 col-md-4">
                 <div className="store_info">
                   <div className="store_name">
                     {this.state.store.store_name}
                   </div>
-                  <br/>
-                  <div className="address">{this.state.store.address}</div>
+
+                  <div className="tags">
+                    {this.state.store.area}
+                    {this.state.category.map((item) => (
+                      <span key={item}>, {item}</span>
+                    ))}
+                  </div>
                   <div className="tel">{this.state.store.tel}</div>
-                  <div className="category">{this.state.store.category}</div>
+                  <div className="score_text">
+                    <ReadScore></ReadScore>
+                  </div>
+                  <div className="liked_item button">
+                    <span>
+                      <Liked></Liked>
+                    </span>
+                    &nbsp;&nbsp;&nbsp;좋아요&nbsp;&nbsp;&nbsp;
+                  </div>
+                  <div className="evaluation button">평가하기</div>
                 </div>
               </div>
-              <div className="store_map"></div>
-              <div className="store_reviews"></div>
             </div>
+            <div>
+              <br />
+              😊 이 음식점의 평가결과는 신뢰할 수 있을 만큼 이루어졌습니다.
+              <br />
+              ※홍보 및 비방 등 부적절한 평가는 평점 산정에서 제외될수있습니다.
+              <br />
+            </div>
+            {/* 리뷰 리스트 */}
+            <div className="store_reviews">
+              <br />
+              <br />
+              <div>리뷰 리스트 들어 갈 자리</div>
+              <br />
+              <br />
+            </div>
+            <div className="store_detail_info">
+              <div className="address">🚩 {this.state.store.address}</div>
+            </div>
+            <div className="store_map"></div>
           </div>
         </div>
         <AppBar></AppBar>
