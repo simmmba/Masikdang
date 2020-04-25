b6ce2757a4496ade52182dd8df07e9e3ba3afd6b
ssongda9412 <ssongda9412@naver.com>
2020-04-26 01:29:33 +0900

    0426 | profile img change | DE, S02P23A201-73
    Former-commit-id: 249ffcf4f70e3b692355463a053cdbc8bf41586d

---
+++ frontend/src/components/common/TextSlider.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Loading.scss";
+
+class TextSlider extends React.Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    return <></>;
+  }
+}
+
+export default TextSlider;
